5d9c3fcd7bb6422005bf362da416569eeb41007e
Moss <danielsmoss96@gmail.com>
2022-06-09 12:35:59 +0100

    Refactor

--- scripts/app.js
+++ scripts/app.js
@@ -8,51 +8,51 @@ class Pokemon {
     }
 
     isEffectiveAgainst(enemy) {
-        if (this.type === "fire" && enemy.type === "grass") return true
-        if (this.type === "water" && enemy.type === "fire") return true
-        if (this.type === "grass" && enemy.type === "water") return true
-    return false
+        if (this.type === "fire" && enemy.type === "grass") return true;
+        if (this.type === "water" && enemy.type === "fire") return true;
+        if (this.type === "grass" && enemy.type === "water") return true;
+        return false;
     } 
 
     isWeakTo(enemy) {
-        if (this.type === "normal" || enemy.type === "normal") return false
+        if (this.type === "normal" || enemy.type === "normal") return false;
         if (this.type === enemy.type) return false;
-        return !this.isEffectiveAgainst(enemy)
+        return !this.isEffectiveAgainst(enemy);
     }
 
     takeDamage(damage) {
-        this.hitPoints -= damage
+        this.hitPoints -= damage;
     }
 
     useMove() {
         console.log(`${this.name} used ${this.move}.`);
-        return this.attackDamage
+        return this.attackDamage;
     }
 
     hasFainted() {
-        if (this.hitPoints > 0) return false
-        return true
+        if (this.hitPoints > 0) return false;
+        return true;
     }
 }
 
 class Fire extends Pokemon {
     constructor(name) {
-        super(name)
-        this.type = 'fire'
+        super(name);
+        this.type = 'fire';
     }
 }
 
 class Water extends Pokemon {
     constructor(name) {
-        super(name)
-        this.type = 'water'
+        super(name);
+        this.type = 'water';
     }
 }
 
 class Grass extends Pokemon {
     constructor(name) {
-        super(name)
-        this.type = 'grass'
+        super(name);
+        this.type = 'grass';
     }
 }
 
