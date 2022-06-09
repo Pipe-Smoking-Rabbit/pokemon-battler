499e7693ab15aa16e342a04421ccf974d735c8d4
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-06-09 12:15:10 +0100

    methods implemented

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,28 +1,71 @@
-const pokemon = require('../scripts/app');
+const parent = require("../scripts/app");
 
-describe('Pokemon Class', () => {
-    test('Should return an instance of the Pokemon class', () => {
-        const basicMon = new pokemon.Pokemon();
-        expect(basicMon).toBeInstanceOf(pokemon.Pokemon);
-    })
-    test('Returned instance has the name property', () => {
-        const mitchsMon = new pokemon.Pokemon('Ian');
-        expect(mitchsMon.name).toBe('Ian');
-    })
-    test('Returned instance has the type property, defaulting to "normal"', () => {
-        const mitchsMon = new pokemon.Pokemon('Ian');
-        expect(mitchsMon.type).toBe('normal');
-    })
-    test('Returned instance has the hitPoints property', () => {
-        const mitchsMon = new pokemon.Pokemon('Ian');
-        expect(mitchsMon.hasOwnProperty('hitPoints')).toBe(true);
-    })
-    test('Returned instance has the attackDamage property', () => {
-        const mitchsMon = new pokemon.Pokemon('Ian');
-        expect(mitchsMon.hasOwnProperty('attackDamage')).toBe(true);
-    })
-    test('Returned instance has the move property, defaulting to "tackle"', () => {
-        const mitchsMon = new pokemon.Pokemon('Ian');
-        expect(mitchsMon.move).toBe("tackle");
-    })
-})
+describe("Pokemon Class", () => {
+  test("Should return an instance of the Pokemon class", () => {
+    const basicMon = new parent.Pokemon();
+    expect(basicMon).toBeInstanceOf(parent.Pokemon);
+  });
+  test("Returned instance has the name property", () => {
+    const mitchsMon = new parent.Pokemon("Ian");
+    expect(mitchsMon.name).toBe("Ian");
+  });
+  test('Returned instance has the type property, defaulting to "normal"', () => {
+    const mitchsMon = new parent.Pokemon("Ian");
+    expect(mitchsMon.type).toBe("normal");
+  });
+  test("Returned instance has the hitPoints property", () => {
+    const mitchsMon = new parent.Pokemon("Ian");
+    expect(mitchsMon.hasOwnProperty("hitPoints")).toBe(true);
+  });
+  test("Returned instance has the attackDamage property", () => {
+    const mitchsMon = new parent.Pokemon("Ian");
+    expect(mitchsMon.hasOwnProperty("attackDamage")).toBe(true);
+  });
+  test('Returned instance has the move property, defaulting to "tackle"', () => {
+    const mitchsMon = new parent.Pokemon("Ian");
+    expect(mitchsMon.move).toBe("tackle");
+  });
+  describe("methods", () => {
+    test('isEffectiveAgainst method returns a boolean if the pokemon is effective against their opponent, "normal" pokemon are not effective against anything', () => {
+      const dansMon = new parent.Pokemon("Morapungo");
+      const mitchsMon = new parent.Pokemon("Ian");
+      expect(dansMon.isEffectiveAgainst(mitchsMon)).toBe(false);
+      dansMon.type = "fire";
+      mitchsMon.type = "grass";
+      expect(dansMon.isEffectiveAgainst(mitchsMon)).toBe(true);
+    });
+    test("isWeakTo method returns boolean if the pokemon is weak to the opponents type, normal are not weak to anything", () => {
+      const dansMon = new parent.Pokemon("Morapungo");
+      const mitchsMon = new parent.Pokemon("Ian");
+      expect(dansMon.isWeakTo(mitchsMon)).toBe(false);
+      dansMon.type = "grass";
+      mitchsMon.type = "fire";
+      expect(dansMon.isWeakTo(mitchsMon)).toBe(true);
+      dansMon.type = "grass";
+      mitchsMon.type = "water";
+      expect(dansMon.isWeakTo(mitchsMon)).toBe(false);
+      dansMon.type = "water";
+      mitchsMon.type = "grass";
+      expect(dansMon.isWeakTo(mitchsMon)).toBe(true);
+    });
+    test("takeDamage method will reduce hitpoints by given number", () => {
+      const dansMon = new parent.Pokemon("Morapungo");
+      dansMon.takeDamage(20);
+      expect(dansMon.hitPoints).toBe(80);
+    });
+    test("useMove method will return attack damage", () => {
+      const dansMon = new parent.Pokemon("Morapungo");
+      const consoleSpy = jest.spyOn(console, "log");
+      expect(dansMon.useMove()).toBe(10);
+      expect(consoleSpy).toHaveBeenCalledWith("Morapungo used tackle.");
+      expect(consoleSpy).toHaveBeenCalledTimes(1);
+      consoleSpy.mockRestore();
+    });
+    test("hasFainted will return a boolean indicating when dead", () => {
+        const dansMon = new parent.Pokemon("Morapungo");
+        expect(dansMon.hasFainted()).toBe(false)
+        dansMon.takeDamage(100)
+        expect(dansMon.hasFainted()).toBe(true)
+    });
+  });
+});
--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,32 @@ class Pokemon {
         this.attackDamage = 10;
         this.move = "tackle";
     }
+
+    isEffectiveAgainst(enemy) {
+        if (this.type === "fire" && enemy.type === "grass") return true
+        if (this.type === "water" && enemy.type === "fire") return true
+        if (this.type === "grass" && enemy.type === "water") return true
+    return false
+    } 
+
+    isWeakTo(enemy) {
+        if (this.type === "normal" || enemy.type === "normal") return false
+        return !this.isEffectiveAgainst(enemy)
+    }
+
+    takeDamage(damage) {
+        this.hitPoints -= damage
+    }
+
+    useMove() {
+        console.log(`${this.name} used ${this.move}.`);
+        return this.attackDamage
+    }
+
+    hasFainted() {
+        if (this.hitPoints > 0) return false
+        return true
+    }
 }
 
 
