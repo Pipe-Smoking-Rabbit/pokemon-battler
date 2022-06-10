a3937ddfb80f0c6805d69a6a57185ce7ffe41e67
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-06-10 12:58:47 +0100

    vanilla game completed

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -1,22 +1,35 @@
 const { Trainer } = require("../scripts/trainer");
 const { Pokeball } = require("../scripts/pokeball");
-const { Pokemon, Fire, Water, Grass, Charmander, Squirtle, Bulbasaur, Rattata } = require("../scripts/monsters");
-const { instantiateBattle } = require("../scripts/battle")
+const {
+  Pokemon,
+  Fire,
+  Water,
+  Grass,
+  Charmander,
+  Squirtle,
+  Bulbasaur,
+  Rattata,
+} = require("../scripts/monsters");
+const { instantiateBattle, takeTurn } = require("../scripts/battle");
 
-describe('battle function', () => {
-    describe('fight -> instantiateBattle', () => {
-        test('Returns the chosen pokemon from the belt of the trainer on which the function was called', () => {
-          const testTrainer = new Trainer("Keith");
-          const testEnemy = new Trainer("Bob");
-          const squirtle = new Squirtle('Squirtle')
-          const charmander = new Charmander('Charmander')
-          const consoleSpy = jest.spyOn(console, 'log')
+describe("battle function", () => {
+  describe("fight -> instantiateBattle", () => {
+    test("takeTurn reduces HP of defending pokemon by the attack damage of attacking pokemon, with damage multiplier applied if necessary", () => {
+      const squirtle = new Squirtle("Squirtle");
+      const charmander = new Charmander("Charmander");
+      takeTurn(squirtle, charmander, true);
+      expect(charmander.hitPoints).toBe(100 - 12.5);
+      takeTurn(charmander, squirtle);
+      expect(squirtle.hitPoints).toBe(100 - 7.5);
+    });
+    test("takeTurn return the opposite of the boolean passed as an argument", () => {
+      const squirtle = new Squirtle("Squirtle");
+      const charmander = new Charmander("Charmander");
+      expect(takeTurn(squirtle, charmander, true)).toBe(false);
+      expect(takeTurn(squirtle, charmander, false)).toBe(true);
+    });
+  });
+});
 
-          testTrainer.catch(squirtle)
-          testEnemy.catch(charmander)
-          testTrainer.fight(testEnemy)
-          //Assume user inputs correctly
-          expect(consoleSpy).toBe("GO SQUIRTLE!!\nGO CHARMANDER!!")
-        })
-    })
-})
+const testTrainer = new Trainer("Keith");
+const testEnemy = new Trainer("Bob");
